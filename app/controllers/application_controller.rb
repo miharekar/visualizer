7bcbdfd851283c24f7385abf9f725d819d09d5a0
Miha Rekar <info@mr.si>
2020-12-18 22:23:59 +0100

    Add profiler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :set_timezone
+  before_action :setup_profiler, :set_timezone
 
   private
+
+  def setup_profiler
+    Rack::MiniProfiler.authorize_request if current_user && current_user.email == "miha@mr.si"
+  end
 
   def set_timezone
     return unless cookies.key?("browser.timezone")
