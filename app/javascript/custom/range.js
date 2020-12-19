66e236f8465101875b7610d1dd43acfb135194af
Miha Rekar <info@mr.si>
2020-12-19 18:15:03 +0100

    Show dot in edit

--- app/javascript/custom/range.js
+++ app/javascript/custom/range.js
@@ -1,11 +1,11 @@
 document.addEventListener("turbolinks:load", function (xhr) {
   if (document.getElementsByClassName("shot-range").length > 0) {
     Array.from(document.getElementsByClassName("shot-range")).forEach((el) => {
-      target = el.parentElement.getElementsByClassName("shot-range-target")[0]
+      dot = el.parentElement.getElementsByClassName("dot")[0]
+      number = el.parentElement.getElementsByClassName("number")[0]
       el.addEventListener("input", function () {
-        target.value = el.value
-        target.style.color = "#fff"
-        target.style.backgroundColor = "hsl(" + (124 / 100 * parseInt(el.value)) + ", 70%, 50%)"
+        number.innerHTML = el.value
+        dot.style.backgroundColor = "hsl(" + (124 / 100 * parseInt(el.value)) + ", 70%, 50%)"
       })
     });
   }
