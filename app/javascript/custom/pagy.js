d495ff92fb174c916db291a29239b7c584527c65
Miha Rekar <info@mr.si>
2020-12-20 20:25:23 +0100

    Add loader

--- app/javascript/custom/pagy.js
+++ app/javascript/custom/pagy.js
@@ -4,9 +4,10 @@ function pagyRequest() {
   pagy.innerHTML += response.html
 
   const loadMoreLink = document.getElementById("pagy-load-more")
+  document.getElementById("pagy-loader").classList.add("d-none")
   if (response.next !== null) {
     loadMoreLink.dataset.page = response.next
-    loadMoreLink.style.opacity = 1
+    loadMoreLink.classList.remove("d-none")
   } else {
     loadMoreLink.parentNode.parentNode.removeChild(loadMoreLink.parentNode);
   }
@@ -31,7 +32,8 @@ function loadNextPage(page, params) {
 
 function loadMore(event) {
   event.preventDefault()
-  event.target.style.opacity = 0
+  event.target.classList.add("d-none")
+  document.getElementById("pagy-loader").classList.remove("d-none")
   const data = event.target.dataset
   loadNextPage(data.page, JSON.parse(data.params))
 }
