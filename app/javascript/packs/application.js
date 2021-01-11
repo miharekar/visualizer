4de223be19f2ab321eca028e7505d8f1770d1fcf
Miha Rekar <info@mr.si>
2021-01-11 10:28:21 +0100

    Add ujs back

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("@rails/ujs").start()
 import "@hotwired/turbo-rails"
 
 // require("channels")
