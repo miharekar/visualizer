e78ecda04fcc184d22a7c377d9ad4567829b3a2d
Miha Rekar <info@mr.si>
2021-01-20 18:22:26 +0100

    THIS DOESN'T WORK

--- app/assets/javascripts/controllers/menu_controller.js
+++ app/assets/javascripts/controllers/menu_controller.js
@@ -1,11 +1,35 @@
+
 import { Controller } from "stimulus"
+// import the enter leave functions
+import { enter, leave } from "el-transition";
 
 export default class extends Controller {
-  static targets = ["toggleable"]
+  static targets = ["toggleable", "transformable", "button"]
+
+  initialize() {
+    this.menuShown = false
+  }
 
   toggle() {
     this.toggleableTargets.forEach((element) => {
       element.classList.toggle("hidden")
     })
+  }
+
+  transform() {
+    if (this.menuShown) {
+      enter(this.transformableTarget)
+    } else {
+      leave(this.transformableTarget)
+    }
+    this.menuShown = !menuClassList.contains("hidden")
+  }
+
+  hide(event) {
+    const buttonClicked = this.buttonTarget.contains(event.target)
+
+    if (!buttonClicked && this.menuShown) {
+      this.transform(this.transformableTarget)
+    }
   }
 }
