d9c47fca206b34d16d6e7d5c1ca8e3a945f22b96
Miha Rekar <info@mr.si>
2021-03-19 12:24:08 +0100

    Sync charts

--- app/javascript/charts/shot.js
+++ app/javascript/charts/shot.js
@@ -1,11 +1,73 @@
 import Highcharts from "highcharts"
 
+function isObject(obj) {
+  return obj && typeof obj === 'object'
+}
+
+Highcharts.wrap(Highcharts.Chart.prototype, 'zoom', function (proceed) {
+  proceed.apply(this, [].slice.call(arguments, 1))
+
+  if (!isObject(this.resetZoomButton)) {
+    Highcharts.charts.forEach(function (chart) {
+      if (isObject(chart.resetZoomButton)) {
+        chart.resetZoomButton.destroy()
+        chart.resetZoomButton = undefined
+      }
+    })
+  }
+})
+
+function handleMouse(e) {
+  const currentChart = this
+  Highcharts.charts.forEach(function (chart) {
+    if (currentChart === chart.renderTo) return
+
+    const event = chart.pointer.normalize(e)
+    let point
+    for (let j = 0; j < chart.series.length && !point; ++j) {
+      point = chart.series[j].searchPoint(event, true)
+    }
+    if (!point) return
+
+    if (e.type === "mousemove") {
+      point.plotY = chart.chartHeight / 2
+      point.onMouseOver()
+      chart.xAxis[0].drawCrosshair(event, point)
+    } else {
+      point.onMouseOut()
+      chart.tooltip.hide(point)
+      chart.xAxis[0].hideCrosshair()
+    }
+  })
+}
+
+function syncExtremes(e) {
+  var thisChart = this.chart
+
+  if (e.trigger !== "syncExtremes") {
+    Highcharts.charts.forEach(function (chart) {
+      if (chart === thisChart) return
+
+      if (chart.xAxis[0].setExtremes) {
+        chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, { trigger: "syncExtremes" })
+        if (!isObject(chart.resetZoomButton)) {
+          chart.showResetZoom()
+        }
+      }
+
+    })
+  }
+}
+
 const chartOptions = {
   xAxis: {
     type: "datetime",
     dateTimeLabelFormats: {
       day: "",
       second: "%M:%S",
+    },
+    events: {
+      setExtremes: syncExtremes
     },
     crosshair: true,
     plotLines: window.shotStages
@@ -86,10 +148,16 @@ function drawTemperatureChart() {
 }
 
 document.addEventListener("turbo:load", function () {
-  if (document.getElementById("shot-chart")) {
+  const shotChart = document.getElementById("shot-chart")
+  const temperatureChart = document.getElementById("temperature-chart")
+  if (shotChart) {
     drawShotChart()
+    shotChart.addEventListener("mousemove", handleMouse)
+    shotChart.addEventListener("mouseleave", handleMouse)
   }
-  if (document.getElementById("temperature-chart")) {
+  if (temperatureChart) {
     drawTemperatureChart()
+    temperatureChart.addEventListener("mousemove", handleMouse)
+    temperatureChart.addEventListener("mouseleave", handleMouse)
   }
 })
