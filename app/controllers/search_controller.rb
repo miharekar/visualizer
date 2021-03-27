7fe94cef815cca79e992b787cb9e97c0196cca21
Miha Rekar <info@mr.si>
2021-03-27 08:00:55 +0100

    Add search by profile title

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,20 +3,25 @@
 class SearchController < ApplicationController
   include Pagy::Backend
 
+  FILTERS = %i[profile_title bean_brand bean_type].freeze
+
   before_action :authenticate_user!
 
   def index
-    @shots = Shot.visible
+    @shots = Shot.visible.with_avatars.by_start_time
 
-    %i[bean_brand bean_type].each do |method|
-      unique_values = Rails.cache.fetch("#{@shots.cache_key_with_version}/#{method}") { @shots.distinct.pluck(method).compact }
-      instance_variable_set("@#{method.to_s.pluralize}", unique_values.sort_by(&:downcase))
+    @filters = {}
+    FILTERS.each do |filter|
+      @filters[filter] = unique_values_for(filter)
+      @shots = @shots.where("#{filter} ILIKE ?", "%#{params[filter]}%") if params[filter].present?
     end
 
-    @shots = @shots.includes(:user).by_start_time
-    @shots = @shots.where("bean_brand ILIKE ?", "%#{params[:bean_brand]}%") if params[:bean_brand].present?
-    @shots = @shots.where("bean_type ILIKE ?", "%#{params[:bean_type]}%") if params[:bean_type].present?
-
     @pagy, @shots = pagy(@shots)
+  end
+
+  def unique_values_for(filter)
+    Rails.cache.fetch("#{Shot.visible.cache_key_with_version}/#{filter}") do
+      Shot.visible.distinct.pluck(filter).compact.map(&:strip).uniq.sort_by(&:downcase)
+    end
   end
 end
