f897716b34288657e455547ab002bee21e20227b
Miha Rekar <info@mr.si>
2021-04-23 21:02:55 +0200

    Rescue from ActiveStorage::IntegrityError

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -7,5 +7,5 @@ class ApplicationJob < ActiveJob::Base
   retry_on StandardError, wait: :exponentially_longer, attempts: 3
 
   # Most jobs are safe to ignore if the underlying records are no longer available
-  discard_on ActiveJob::DeserializationError
+  discard_on ActiveJob::DeserializationError, ActiveStorage::IntegrityError
 end
