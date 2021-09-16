bad701e7958f872c2adf2924b586049d4c639a86
Miha Rekar <info@mr.si>
2021-09-16 11:21:00 +0200

    Update rails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
