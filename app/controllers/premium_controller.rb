a308c0c132356a1dccff4bfef7bda388fb35bdee
Miha Rekar <info@mr.si>
2021-12-05 12:12:55 +0100

    Add Visualizer Plans

--- app/controllers/premium_controller.rb
+++ app/controllers/premium_controller.rb
@@ -8,6 +8,7 @@ class PremiumController < ApplicationController
   end
 
   def create
+    price_id = Stripe::Price.list(active: true, recurring: {interval: "month"}).first.id
     session = Stripe::Checkout::Session.create(
       success_url: "#{success_premium_index_url}?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: cancel_premium_index_url,
@@ -15,10 +16,7 @@ class PremiumController < ApplicationController
       customer_email: current_user.email,
       automatic_tax: {enabled: true},
       metadata: {user_id: current_user.id},
-      line_items: [{
-        quantity: 1,
-        price: ENV["STRIPE_PRICE"]
-      }]
+      line_items: [{quantity: 1, price: price_id}]
     )
     redirect_to session.url, allow_other_host: true
   end
