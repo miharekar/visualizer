bd0ed2811b6f80d71eb8bad277c6a1f1cf619048
Miha Rekar <info@mr.si>
2022-01-12 14:55:20 +0100

    Improve sluggable

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -6,38 +6,38 @@ module Sluggable
   class_methods do
     attr_reader :slug_source
 
+    def find_by_slug_or_id(slug_or_id)
+      find_by(slug: slug_or_id) || find_by(id: slug_or_id)
+    end
+
+    def find_by_slug_or_id!(slug_or_id)
+      find_by(slug: slug_or_id) || find(slug_or_id)
+    end
+
     def slug_from(column)
       @slug_source = column
     end
   end
 
   included do
-    before_save :set_slug
+    before_validation :generate_slug
   end
 
   private
 
-  def set_slug
-    return if slug_source.blank? || slug.present?
+  def generate_slug
+    return if slug.present?
 
     self.slug = unique_slug
   end
 
-  def slug_source
-    public_send(self.class.slug_source)
-  end
-
   def unique_slug
-    slug = slug_source.to_s.parameterize
-    return slug unless slug_taken?(slug)
+    slug = public_send(self.class.slug_source).to_s.parameterize
+    return slug unless self.class.exists?(slug:)
 
     (2..).each do |i|
       new_slug = "#{slug}-#{i}"
-      break new_slug unless slug_taken?(new_slug)
+      break new_slug unless self.class.exists?(slug: new_slug)
     end
-  end
-
-  def slug_taken?(slug)
-    self.class.exists?(slug:)
   end
 end
---
+++ test/models/concerns/sluggable_test.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SluggableDummy
+  def self.exists?(_)
+    if @num_exists.positive?
+      @num_exists -= 1
+      return true
+    end
+    false
+  end
+
+  def self.before_validation(_); end
+
+  def self.validates(*_args); end
+
+  include Sluggable
+  slug_from :title
+
+  attr_reader :options
+
+  def initialize(num_exists, **options)
+    self.class.instance_variable_set(:@num_exists, num_exists)
+    @options = options
+  end
+
+  def method_missing(method, *_args, **_options, &)
+    return options[method] if respond_to_missing?(method)
+
+    super
+  end
+
+  def respond_to_missing?(method)
+    options.key?(method)
+  end
+end
+
+class SluggableTest < ActiveSupport::TestCase
+  test "it sets the slug" do
+    dummy = SluggableDummy.new(0, title: "aa bb")
+    assert_equal "aa-bb", dummy.__send__(:unique_slug)
+  end
+
+  test "sets the slug when there are already instances with that slug" do
+    dummy = SluggableDummy.new(3, title: "aa bb")
+    assert_equal "aa-bb-4", dummy.__send__(:unique_slug)
+  end
+end
