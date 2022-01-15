768e00faf5fb70600a544a0a3aba5bcff342ebac
Miha Rekar <info@mr.si>
2022-01-15 17:59:55 +0100

    Fix resistance formula, add conductance and conductance diff

--- app/models/shot_chart.rb
+++ app/models/shot_chart.rb
@@ -5,6 +5,7 @@ class ShotChart
 
   DATA_LABELS_TO_IGNORE = %w[espresso_resistance espresso_resistance_weight espresso_state_change].freeze
   MAX_RESISTANCE_VALUE = 19
+  MIN_CONDUCTANCE_DIFF_VALUE = -5
   CHART_SETTINGS = {
     "espresso_pressure" => {"title" => "Pressure", "color" => "#05c793", "suffix" => " bar", "type" => "spline"},
     "espresso_pressure_goal" => {"title" => "Pressure Goal", "color" => "#03634a", "suffix" => " bar", "dashed" => true, "type" => "spline"},
@@ -13,7 +14,9 @@ class ShotChart
     "espresso_flow" => {"title" => "Flow", "color" => "#1fb7ea", "suffix" => " ml/s", "type" => "spline"},
     "espresso_flow_weight" => {"title" => "Weight Flow", "color" => "#8f6400", "suffix" => " g/s", "type" => "spline"},
     "espresso_flow_goal" => {"title" => "Flow Goal", "color" => "#09485d", "suffix" => " ml/s", "dashed" => true, "type" => "spline"},
-    "espresso_resistance" => {"title" => "Resistance", "color" => "#e5e500", "suffix" => " lΩ", "hidden" => true, "type" => "spline"},
+    "espresso_resistance" => {"title" => "Resistance", "color" => "#e5e500", "suffix" => " lΩ", "hidden" => false, "type" => "spline"},
+    "espresso_conductance" => {"title" => "Conductance", "color" => "#f36943", "suffix" => "", "hidden" => true, "dashed" => true, "type" => "spline"},
+    "espresso_conductance_derivative" => {"title" => "Conductance Derivative", "color" => "#f36943", "suffix" => "", "hidden" => true, "type" => "spline"},
     "espresso_temperature_basket" => {"title" => "Temperature Basket", "color" => "#e73249", "suffix" => " °C", "type" => "spline"},
     "espresso_temperature_mix" => {"title" => "Temperature Mix", "color" => "#ce123e", "suffix" => " °C", "type" => "spline"},
     "espresso_temperature_goal" => {"title" => "Temperature Goal", "color" => "#960d2d", "suffix" => " °C", "dashed" => true, "type" => "spline"}
@@ -46,6 +49,8 @@ class ShotChart
   def prepare_chart_data
     @processed_shot_data = process_data(shot)
     @processed_shot_data["espresso_resistance"] = resistance_chart(@processed_shot_data["espresso_pressure"], @processed_shot_data["espresso_flow"])
+    @processed_shot_data["espresso_conductance"] = conductance_chart(@processed_shot_data["espresso_pressure"], @processed_shot_data["espresso_flow"])
+    @processed_shot_data["espresso_conductance_derivative"] = conductance_derivative_chart(@processed_shot_data["espresso_conductance"])
   end
 
   def for_highcharts(data)
@@ -82,11 +87,36 @@ class ShotChart
       if f.zero?
         v = nil
       else
-        r = v.to_f / f
+        r = v.to_f / (f**2)
         v = r > MAX_RESISTANCE_VALUE ? nil : r
       end
       [t, v]
     end
+  end
+
+  def conductance_chart(pressure_data, flow_data)
+    pressure_data.map.with_index do |(t, v), i|
+      f = flow_data[i].second.to_f
+      if v.zero?
+        v = nil
+      else
+        c = (f**2) / v.to_f
+        v = c > MAX_RESISTANCE_VALUE ? nil : c
+      end
+      [t, v]
+    end
+  end
+
+  def conductance_derivative_chart(conductance_data)
+    derivative = []
+    conductance_data.each_cons(2) do |(t1, v1), (t2, v2)|
+      next if v1.nil? || v2.nil?
+
+      v = ((v2 - v1) / ((t2 - t1) / 1000)) * 10
+      v = nil if v > MAX_RESISTANCE_VALUE || v < MIN_CONDUCTANCE_DIFF_VALUE
+      derivative << [t1, v]
+    end
+    derivative
   end
 
   def stages_from_state_change(data)
