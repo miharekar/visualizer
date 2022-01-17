f7b7a7e6a65bf0ddd97d7a3aca1ede62776d6aa5
Miha Rekar <info@mr.si>
2022-01-17 13:40:53 +0100

    Show total users growth in stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -14,9 +14,19 @@ class StatsController < ApplicationController
       name: "Shots brewed on day",
       data: Shot.where(start_time: (("1.1.2020".to_date)..Time.zone.today)).order("start_time::date").group("start_time::date").count.map { |date, count| [date.to_time.to_i * 1000, count] }
     }]
+    user_creations = User.order(:created_at).pluck(:created_at)
+    user_data = {}
+    total_users = 0
+    user_creations.each do |creation|
+      total_users += 1
+      user_data[creation.to_date.to_time.to_i * 1000] = total_users
+    end
     @user_chart = [{
       name: "User joins",
       data: User.order("created_at::date").group("created_at::date").count.map { |date, count| [date.to_time.to_i * 1000, count] }
+    }, {
+      name: "Total users",
+      data: user_data.to_a
     }]
   end
 end
