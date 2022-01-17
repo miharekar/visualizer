4adc2527f0b36c390a036955faf161a242c649d8
Miha Rekar <info@mr.si>
2022-01-17 13:43:48 +0100

    Update deps

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,7 @@ pin "@rails/actioncable", to: "actioncable.esm.js"
 pin "@hotwired/turbo-rails", to: "turbo.js"
 pin "@hotwired/stimulus", to: "stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "stimulus-autocomplete", to: "https://cdn.jsdelivr.net/npm/stimulus-autocomplete@3.0.0-rc.7/src/autocomplete.js"
+pin "stimulus-autocomplete", to: "https://cdn.jsdelivr.net/npm/stimulus-autocomplete@3.0.1/src/autocomplete.js"
 pin "el-transition", to: "https://cdn.jsdelivr.net/npm/el-transition@0.0.7/index.js"
 pin "highcharts", to: "https://cdn.jsdelivr.net/npm/highcharts@9.3.2/es-modules/masters/highcharts.src.js"
 
