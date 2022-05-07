819e2ba550dc84dd3e2d43dc7170ac4f90d67230
Miha Rekar <info@mr.si>
2022-05-07 17:06:52 +0200

    Add EY calculator

---
+++ app/javascript/controllers/ey_calculator_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["drinkWeight", "beanWeight", "tds", "ey"]
+
+  calculate() {
+    const drinkWeight = this.getFloatFromString(this.drinkWeightTarget.value)
+    const beanWeight = this.getFloatFromString(this.beanWeightTarget.value)
+    const tds = this.getFloatFromString(this.tdsTarget.value)
+    const ey = drinkWeight * tds / beanWeight
+    this.eyTarget.value = ey.toFixed(2)
+  }
+
+  getFloatFromString(string) {
+    return parseFloat(string.replace(',', '.').replace(' ', ''))
+  }
+}
