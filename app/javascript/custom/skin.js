e3a627978818e61c34b8ff72b3ebb1c6ee3df543
Miha Rekar <info@mr.si>
2022-05-29 09:56:59 +0200

    Detect color scheme change and set cookie immediately

--- app/javascript/custom/skin.js
+++ app/javascript/custom/skin.js
@@ -1,11 +1,10 @@
 function setDarkMode() {
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.cookie = "browser.colorscheme=dark; path=/"
   } else {
     document.cookie = "browser.colorscheme=light; path=/"
   }
 }
 
-document.addEventListener("turbo:load", function () {
-  setDarkMode()
-})
+document.addEventListener("turbo:load", setDarkMode)
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", setDarkMode)
