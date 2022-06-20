19a512e252e28209083b401d7fd7074ba0130f32
Miha Rekar <info@mr.si>
2022-06-20 11:06:51 +0200

    Move shared/flash to layout

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,8 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
-import { leave } from "el-transition"
 
 export default class extends Controller {
   hide() {
-    leave(this.element)
+    this.element.classList.add("hidden")
   }
 }
