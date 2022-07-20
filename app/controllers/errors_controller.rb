2ed5b29fb0ee19d2efbd307cb3f31105c052b37f
Miha Rekar <info@mr.si>
2022-07-20 11:04:31 +0200

    Also set http statuses in errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ErrorsController < ApplicationController
+  def not_found
+    render status: :not_found
+  end
+
+  def internal_server_error
+    render status: :internal_server_error
+  end
 end
