2b39e4aadc8f86a06e234a53209901331856187b
Miha Rekar <info@mr.si>
2022-07-20 11:01:20 +0200

    Add custom error pages

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module DecentVisualizer
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.active_storage.analyzers = []
+    config.exceptions_app = routes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -66,4 +66,7 @@ Rails.application.routes.draw do
 
   resources :stats, only: [:index]
   resources :changes, except: %i[index destroy]
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
