5187e02583f08f9e5db6d73624a72406670d6ac6
Miha Rekar <info@mr.si>
2022-08-09 09:52:13 +0200

    This is really taxing for the db

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -16,9 +16,6 @@ class StatsController < ApplicationController
     }]
 
     @user_chart = user_chart
-    @top_profiles_all_time = top_profiles(from: "1.1.2000".to_date)
-    @top_profiles_last_month = top_profiles(from: 1.month.ago)
-    @top_skins = top_skins
   end
 
   private
@@ -38,18 +35,5 @@ class StatsController < ApplicationController
       name: "Total users",
       data: user_data.to_a
     }]
-  end
-
-  def top_profiles(from:)
-    profiles = Shot.where(created_at: from..).pluck(:profile_title)
-    total = profiles.size.to_f
-    profiles.map { |p| p.to_s.sub("Visualizer/", "") }.tally.sort_by { |_p, c| c }.last(20).reverse.to_h.transform_values { |c| (c / total * 100).round(2) }
-  end
-
-  def top_skins
-    relevant = Shot.where(created_at: "27.06.2022"..).pluck(:extra, :user_id)
-    users_with_skins = relevant.map { |e, u| [u, e["skin"]] }.uniq.select { |_, s| s.present? }
-    total_users = users_with_skins.uniq { |us| us[0] }.size.to_f
-    users_with_skins.map { |us| us[1] }.tally.sort_by { |_s, c| c }.reverse.to_h.transform_values { |c| (c / total_users * 100).round(2) }
   end
 end
