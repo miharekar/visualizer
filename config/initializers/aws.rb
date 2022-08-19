c4c6f5741e919502f5d7b3f2eeba638fcf2bb5de
Miha Rekar <info@mr.si>
2022-08-19 08:38:19 +0200

    Only set config if it's present

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,10 +2,12 @@
 
 require "aws-sdk-s3"
 
-Aws.config.update(
-  credentials: Aws::Credentials.new(
-    Rails.application.credentials.aws.access_key_id,
-    Rails.application.credentials.aws.secret_access_key
-  ),
-  region: Rails.application.credentials.aws.region
-)
+if Rails.application.credentials.aws
+  Aws.config.update(
+    credentials: Aws::Credentials.new(
+      Rails.application.credentials.aws.access_key_id,
+      Rails.application.credentials.aws.secret_access_key
+    ),
+    region: Rails.application.credentials.aws.region
+  )
+end
--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-Cloudinary.config do |config|
-  config.cloud_name = Rails.application.credentials.cloudinary.cloud_name
-  config.api_key = Rails.application.credentials.cloudinary.api_key
-  config.api_secret = Rails.application.credentials.cloudinary.secret
-  config.secure = true
+if Rails.application.credentials.cloudinary
+  Cloudinary.config do |config|
+    config.cloud_name = Rails.application.credentials.cloudinary.cloud_name
+    config.api_key = Rails.application.credentials.cloudinary.api_key
+    config.api_secret = Rails.application.credentials.cloudinary.secret
+    config.secure = true
+  end
 end
