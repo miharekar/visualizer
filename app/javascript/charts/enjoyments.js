160e8f78f04cb2db14cf985138c2c82196f3dab5
Miha Rekar <info@mr.si>
2022-10-23 20:29:01 +0200

    Improve enjoyments chart

--- app/javascript/charts/enjoyments.js
+++ app/javascript/charts/enjoyments.js
@@ -1,50 +1,86 @@
 import Highcharts from "highcharts"
 import { Turbo } from "@hotwired/turbo-rails"
 
-const options = {
-  accessibility: { enabled: false },
-  animation: false,
-  title: false,
-  legend: { enabled: false },
-  chart: {
-    zoomType: "x",
-    height: 300
-  },
-  xAxis: { type: "datetime", },
-  title: false,
-  yAxis: { title: false },
-  credits: { enabled: false },
-  tooltip: {
+function isDark() {
+  if (document.body.classList.contains("system")) {
+    return window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+  } else {
+    return document.body.classList.contains("dark")
+  }
+}
+
+function getColors() {
+  if (isDark()) {
+    return {
+      background: "#000000",
+      gridLine: "#191919"
+    }
+  }
+  else {
+    return {
+      background: "#ffffff",
+      gridLine: "#e6e6e6"
+    }
+  }
+}
+
+function commonOptions() {
+  const colors = getColors()
+  return {
+    accessibility: { enabled: false },
     animation: false,
-    formatter: function () {
-      return this.point.title
-    }
-  },
-  plotOptions: {
-    series: {
-      cursor: "pointer",
-      point: {
-        events: {
-          click: function () {
-            Turbo.visit(this.url)
-          }
-        }
-      },
-      states: { hover: { enabled: false } },
+    title: false,
+    legend: { enabled: false },
+    chart: {
+      zoomType: "x",
+      height: 300,
+      backgroundColor: colors.background
+    },
+    xAxis: {
+      type: "datetime",
+      gridLineColor: colors.gridLine,
+      lineColor: colors.line,
+      tickColor: colors.line,
+    },
+    title: false,
+    yAxis: {
+      title: false,
+      max: 100,
+      gridLineColor: colors.gridLine
+    },
+    credits: { enabled: false },
+    tooltip: {
       animation: false,
-      marker: {
-        enabled: true,
-        radius: 5
-      },
-      lineWidth: 0
-    }
-  },
+      formatter: function () {
+        return this.point.title
+      }
+    },
+    plotOptions: {
+      series: {
+        cursor: "pointer",
+        point: {
+          events: {
+            click: function () {
+              Turbo.visit(this.url)
+            }
+          }
+        },
+        states: { hover: { enabled: false } },
+        animation: false,
+        marker: {
+          enabled: true,
+          radius: 5
+        },
+        lineWidth: 0
+      }
+    },
+  }
 }
 
 function drawEnjoymentsChart() {
   if (document.getElementById("enjoyments-chart")) {
     const chartOptions = {
-      ...options,
+      ...commonOptions(),
       series: [{
         name: "Enjoyments throughout time",
         data: window.enjoymentsData,
@@ -57,3 +93,9 @@ function drawEnjoymentsChart() {
 
 document.addEventListener("turbo:frame-load", drawEnjoymentsChart)
 document.addEventListener("turbo:load", drawEnjoymentsChart)
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function () {
+  if (document.body.classList.contains("system")) {
+    drawEnjoymentsChart()
+  }
+})
