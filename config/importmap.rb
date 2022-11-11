f110861ed9314b439add71c651555002178b8ccd
Miha Rekar <info@mr.si>
2022-11-11 10:30:01 +0100

    Modernize importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-pin "application"
-pin "@rails/activestorage", to: "activestorage.esm.js"
-pin "@rails/actioncable", to: "actioncable.esm.js"
-pin "@hotwired/turbo-rails", to: "turbo.js"
-pin "@hotwired/stimulus", to: "stimulus.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "stimulus-autocomplete", to: "https://ga.jspm.io/npm:stimulus-autocomplete@3.0.2/src/autocomplete.js"
-pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
+pin "application", preload: true
+pin "@rails/activestorage", to: "activestorage.esm.js", preload: true
+pin "@rails/actioncable", to: "actioncable.esm.js", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "stimulus-autocomplete", to: "https://ga.jspm.io/npm:stimulus-autocomplete@3.0.2/src/autocomplete.js", preload: true
+pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js", preload: true
 pin "highcharts", to: "https://code.highcharts.com/es-modules/masters/highcharts.src.js"
 pin "highcharts-annotations", to: "https://code.highcharts.com/es-modules/masters/modules/annotations.src.js"
 
