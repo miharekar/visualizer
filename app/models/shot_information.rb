1e2bc852003ebc988fa640586176729505685ca9
Miha Rekar <info@mr.si>
2022-11-15 08:00:03 +0100

    Remove watermark if it's there already

--- app/models/shot_information.rb
+++ app/models/shot_information.rb
@@ -41,7 +41,7 @@ class ShotInformation < ApplicationRecord
 
     content = tcl_profile_fields.to_a.sort_by(&:first).map do |k, v|
       v = "Visualizer/#{v}" if k == "profile_title"
-      v = "#{v}\n\nDownloaded from Visualizer" if k == "profile_notes"
+      v = "#{v.gsub(/Downloaded from Visualizer/, "").strip}\n\nDownloaded from Visualizer" if k == "profile_notes"
       v = "{}" if v.blank?
       v = "{#{v}}" if /\w\s\w/.match?(v)
 
@@ -58,7 +58,7 @@ class ShotInformation < ApplicationRecord
     JSON_PROFILE_KEYS.each do |key|
       v = profile_fields["json"][key]
       v = "Visualizer/#{v}" if key == "title"
-      v = "#{v}\n\nDownloaded from Visualizer" if key == "notes"
+      v = "#{v.gsub(/Downloaded from Visualizer/, "").strip}\n\nDownloaded from Visualizer" if key == "notes"
       json[key] = v
     end
 
