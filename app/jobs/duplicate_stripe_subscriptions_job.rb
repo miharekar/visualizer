7bf32cd732d4a03a92f332bf49f822d38aeb76eb
Miha Rekar <info@mr.si>
2022-11-19 11:41:20 +0100

    Detect duplicate subscriptions

---
+++ app/jobs/duplicate_stripe_subscriptions_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class DuplicateStripeSubscriptionsJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    active_subscriptions = {}
+    Stripe::Customer.list(limit: 100, expand: ["data.subscriptions"]).auto_paging_each do |customer|
+      customer.subscriptions.each do |subscription|
+        if subscription.status == "active"
+          active_subscriptions[customer.email] ||= []
+          active_subscriptions[customer.email] << subscription.id
+        end
+      end
+    end
+    active_subscriptions.select { |email, subscriptions| subscriptions.count > 1 }.each do |email, subscriptions|
+      Sentry.capture_message("Duplicate subscription for #{email}", level: "debug", extra: {subscriptions:})
+    end
+  end
+end
