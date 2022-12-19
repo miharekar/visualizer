5ab665ecd24d40b1bf8ad06cf7e9391f990a087b
Miha Rekar <info@mr.si>
2022-12-19 15:19:07 +0100

    Use the real .count

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,8 +2,4 @@
 
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-
-  def self.fast_count
-    ActiveRecord::Base.connection.execute("SELECT reltuples AS approximate FROM pg_class WHERE relname = '#{table_name}';").first["approximate"].to_i
-  end
 end
