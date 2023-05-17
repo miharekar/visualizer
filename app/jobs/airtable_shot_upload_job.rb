db51ed2df8f31a0cefdadb9aec3392643847f03b
Miha Rekar <info@mr.si>
2023-05-17 16:22:36 +0200

    Destroy identity if we don't have permissions anymore

--- app/jobs/airtable_shot_upload_job.rb
+++ app/jobs/airtable_shot_upload_job.rb
@@ -5,5 +5,12 @@ class AirtableShotUploadJob < ApplicationJob
 
   def perform(shot)
     Airtable::Shots.new(shot.user).upload(shot)
+  rescue Airtable::DataError => e
+    json = JSON.parse(e.message)
+    if json["error"]["type"] == "INVALID_PERMISSIONS_OR_MODEL_NOT_FOUND"
+      shot.user.identities.by_provider(:airtable).first.destroy
+    else
+      RorVsWild.record_error(e, user_id: user.id)
+    end
   end
 end
