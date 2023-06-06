119e3b1f1d3c05f6e89773232d84ff22df105f5f
Miha Rekar <info@mr.si>
2023-06-06 17:13:50 +0200

    Parse revision and server for RorVsWild

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 if Rails.application.credentials[:rorvswild].present?
+  mrsked = ENV["MRSK_CONTAINER_NAME"].to_s.split("-")
+
   RorVsWild.start(
     api_key: Rails.application.credentials[:rorvswild],
     deployment: {
-      revision: ENV["GIT_SHA"],
-      description: ENV["GIT_MESSAGE"]
+      revision: mrsked.try(:[], 2).to_s.split("_").first
+    },
+    server: {
+      name: "#{mrsked.try(:[], 0)}-#{mrsked.try(:[], 1)}-#{Socket.gethostname}"
     }
   )
 end
