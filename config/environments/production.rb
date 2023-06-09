7487de3bad3c7525f3a835ab26b0c4ddf95f3016
Miha Rekar <info@mr.si>
2023-06-09 22:00:19 +0200

    Do not need $redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, {url: ENV.fetch("CACHE_REDIS_URL", nil)}
+  config.cache_store = :redis_cache_store, {url: ENV["CACHE_REDIS_URL"]}
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :sidekiq
--- config/initializers/redis.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-$redis = Redis.new(url: ENV.fetch("REDIS_URL", nil), ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}) # rubocop:disable Style/GlobalVars
