28e8f21309c0d645a191fde83be3e3fcfaa4028e
Miha Rekar <miha@uplisting.io>
2023-10-07 15:02:48 +0200

    Add weekly and daily stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,17 +6,28 @@ class StatsController < ApplicationController
   def index
     @shot_count = Shot.count
     @user_count = User.count
-    @uploaded_chart = [{name: "Uploaded .shot files per week", data: weekly_uploads}]
-    @user_chart = [{
-      name: "User joins",
-      data: user_joins
-    }, {
-      name: "Total users",
-      data: total_users
-    }]
+    @uploaded_chart = [
+      {name: "Uploaded .shot files per day", data: daily_uploads},
+      {name: "Uploaded .shot files per week", data: weekly_uploads}
+    ]
+    @user_chart = [
+      {name: "Daily User joins", data: daily_joins},
+      {name: "Weekly User joins", data: weekly_joins},
+      {name: "Total users", data: total_users}
+    ]
   end
 
   private
+
+  def daily_uploads
+    Shot
+      .with(days: Shot.select("DATE_TRUNC('day', created_at) as day"))
+      .from("days")
+      .group(:day)
+      .count
+      .map { |day, count| [day.to_i * 1000, count] }
+      .sort_by(&:first)
+  end
 
   def weekly_uploads
     Shot
@@ -28,8 +39,24 @@ class StatsController < ApplicationController
       .sort_by(&:first)
   end
 
-  def user_joins
-    User.order(:created_at).group(:created_at).count.map { |date, count| [date.to_i * 1000, count] }
+  def daily_joins
+    User
+      .with(days: User.select("DATE_TRUNC('day', created_at) as day"))
+      .from("days")
+      .group(:day)
+      .count
+      .map { |day, count| [day.to_i * 1000, count] }
+      .sort_by(&:first)
+  end
+
+  def weekly_joins
+    User
+      .with(weeks: User.select("DATE_TRUNC('week', created_at) as week"))
+      .from("weeks")
+      .group(:week)
+      .count
+      .map { |week, count| [week.to_i * 1000, count] }
+      .sort_by(&:first)
   end
 
   def total_users
