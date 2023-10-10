c96e073720f9254a7a4ed46fc3ee9270fe9ebe60
Miha Rekar <miha@uplisting.io>
2023-10-10 22:18:46 +0200

    Bump gems and enable global thread pool

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.active_record.async_query_executor = :global_thread_pool
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
