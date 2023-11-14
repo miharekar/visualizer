42c0970deac10a9ef0168f62c20f8bf07ff50994
Miha Rekar <info@mr.si>
2023-11-14 07:52:53 +0100

    Add /up health check

--- config/routes.rb
+++ config/routes.rb
@@ -83,4 +83,5 @@ Rails.application.routes.draw do
 
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
+  get "/up" => "rails/health#show", :as => :rails_health_check
 end
