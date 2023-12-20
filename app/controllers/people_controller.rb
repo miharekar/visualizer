e94f91c5c3b0d8e02e8484d16dcb4a0124bf6874
Miha Rekar <info@mr.si>
2023-12-20 14:53:21 +0100

    Handle people going private in feeds

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,40 +5,39 @@ class PeopleController < ApplicationController
 
   def show
     @user = User.find_by(slug: params[:id])
-
     if @user.nil?
       @user = User.find_by(id: params[:id])
-      return redirect_to community_index_path, alert: "User #{params[:id]} was not found" if @user.nil?
-      return redirect_to person_path(id: @user.slug), status: :moved_permanently if @user.public?
-    end
-
-    if @user.public
+      if @user.nil?
+        redirect_to community_index_path, alert: "User #{params[:id]} was not found"
+      else
+        redirect_to person_path(id: @user.slug), status: :moved_permanently
+      end
+    elsif !@user.public
+      redirect_to community_index_path, alert: "User #{params[:id]} was not found"
+    else
       @shots = @user.shots
       unless current_user&.premium?
         @premium_count = @shots.premium.count
         @shots = @shots.non_premium
       end
       @shots, @cursor = paginate_with_cursor(@shots, by: :start_time, before: params[:before])
-    else
-      redirect_to :root
     end
   end
 
   def feed
     @user = User.find_by(slug: params[:id])
-
     if @user.nil?
       @user = User.find_by(id: params[:id])
-      return head :not_found if @user.nil?
-      return redirect_to feed_person_path(id: @user.slug), status: :moved_permanently if @user.public?
-    end
-
-    if @user&.public
-      @shots = @user.shots.by_start_time.non_premium.limit(30)
-    else
+      if @user.nil?
+        head :not_found
+      else
+        redirect_to feed_person_path(id: @user.slug), status: :moved_permanently
+      end
+    elsif !@user.public
       head :not_found
+    else
+      @shots = @user.shots.by_start_time.non_premium.limit(30)
+      render formats: :rss
     end
-
-    render formats: :rss
   end
 end
