228b76607f494769e8a93044fdf9738c31572e63
Miha Rekar <info@mr.si>
2023-12-27 17:33:32 +0100

    Make paginate_with_cursor more flexible

--- app/controllers/concerns/cursor_paginatable.rb
+++ app/controllers/concerns/cursor_paginatable.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 module CursorPaginatable
-  def paginate_with_cursor(relation, items: 20, by: :id, before: nil)
+  def paginate_with_cursor(relation, items: 20, before: nil, by: :id, direction: :desc)
     relation = relation.where(by => ..before) if before.present?
-    relation = relation.order(by => :desc).limit(items + 1).load
+    relation = relation.order(by => direction).limit(items + 1).load
     cursor = relation.last.public_send(by) if relation.size > items
 
     [relation.take(items), cursor]
