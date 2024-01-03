89d60ba062ad1b008ea43fd2fb665a04b84ea057
Miha Rekar <info@mr.si>
2024-01-03 08:55:11 +0100

    Wrap ScreenshotTaker in a timeout

--- app/jobs/screenshot_taker_job.rb
+++ app/jobs/screenshot_taker_job.rb
@@ -12,25 +12,27 @@ class ScreenshotTakerJob < ApplicationJob
   def perform(shot)
     return if shot.screenshot? || Rails.env.development? || Rails.env.test?
 
-    chart = ShotChart.new(shot)
-    options = Oj.load(File.read("lib/assets/chart_options.json"))
-    options["xAxis"]["plotLines"] = chart.stages
-    options["series"] = chart.shot_chart
+    Timeout.timeout(10) do
+      chart = ShotChart.new(shot)
+      options = Oj.load(File.read("lib/assets/chart_options.json"))
+      options["xAxis"]["plotLines"] = chart.stages
+      options["series"] = chart.shot_chart
 
-    uri = URI.parse("https://export.highcharts.com/")
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri, {"Content-Type": "application/json"})
-    request.body = {options:, type: "image/png"}.to_json
-    response = http.request(request)
-    File.write("tmp/screenshot-#{shot.id}.png", response.body, mode: "wb")
+      uri = URI.parse("https://export.highcharts.com/")
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Post.new(uri.request_uri, {"Content-Type": "application/json"})
+      request.body = {options:, type: "image/png"}.to_json
+      response = http.request(request)
+      File.write("tmp/screenshot-#{shot.id}.png", response.body, mode: "wb")
 
-    client = Aws::S3::Client.new
-    s3_response = client.put_object(
-      acl: "public-read",
-      body: File.read("tmp/screenshot-#{shot.id}.png"),
-      bucket: "visualizer-coffee-shots",
-      key: "screenshots/#{shot.id}.png"
-    )
-    shot.update(s3_etag: s3_response.etag) if s3_response&.etag
+      client = Aws::S3::Client.new
+      s3_response = client.put_object(
+        acl: "public-read",
+        body: File.read("tmp/screenshot-#{shot.id}.png"),
+        bucket: "visualizer-coffee-shots",
+        key: "screenshots/#{shot.id}.png"
+      )
+      shot.update(s3_etag: s3_response.etag) if s3_response&.etag
+    end
   end
 end
