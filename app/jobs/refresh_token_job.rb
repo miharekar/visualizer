91fac91e603300d043345ffe5e882f75a7bda65e
Miha Rekar <info@mr.si>
2024-03-20 21:35:03 +0100

    Retry PgLock::UnableToLockError

--- app/jobs/refresh_token_job.rb
+++ app/jobs/refresh_token_job.rb
@@ -3,6 +3,8 @@
 class RefreshTokenJob < ApplicationJob
   queue_as :high
 
+  retry_on PgLock::UnableToLockError, wait: :polynomially_longer
+
   def perform(identity)
     identity.refresh_token!
 
