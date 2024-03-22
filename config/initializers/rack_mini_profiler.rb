e5cae9960c3f7d8f18d508dafd2866ca1379d43a
Miha Rekar <info@mr.si>
2024-03-22 15:57:51 +0100

    Stop using REDIS_URL

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -2,5 +2,5 @@
 
 Rack::MiniProfiler.config.enable_hotwire_turbo_drive_support = true
 Rack::MiniProfiler.config.position = "bottom-right"
-Rack::MiniProfiler.config.storage_options = {url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }}
+Rack::MiniProfiler.config.storage_options = {url: ENV.fetch("CACHE_REDIS_URL") { "redis://localhost:6379/1" }}
 Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
