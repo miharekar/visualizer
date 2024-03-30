a0d03d24da507a3bc59dee645e0b82a6b907e9b6
Miha Rekar <info@mr.si>
2024-03-30 10:03:28 +0100

    Rework importmap
    Skypack seems abandoned:
    - https://github.com/skypackjs/skypack-cdn/issues/365
    - https://github.com/skypackjs/skypack-cdn/issues/362
    
    importmap-rails switched logic to always download: https://github.com/rails/importmap-rails/pull/217
    
    But it doesn't work for complex bigger packages. There's a WIP PR to address this: https://github.com/rails/importmap-rails/pull/235
    
    So I decided to use the new rails-importmap approach where I can, and switch to jsdelivr where I have to.
    
    I've pinned to major versions and jsdelivr should take care of everything else. I've also updated the rake task to check for major version updates.

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,12 +5,14 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
-# use rails importmap:update to update these to the latest version
-pin "stimulus-autocomplete", to: "https://cdn.skypack.dev/pin/stimulus-autocomplete@v3.1.0-sAtS84yDhc0OptdElB2O/mode=imports/optimized/stimulus-autocomplete.js" # source: https://cdn.skypack.dev/stimulus-autocomplete
-pin "el-transition", to: "https://cdn.skypack.dev/pin/el-transition@v0.0.7-BadiZFlYOen4UgfYoLiR/mode=imports/optimized/el-transition.js" # source: https://cdn.skypack.dev/el-transition
-pin "highcharts", to: "https://cdn.skypack.dev/pin/highcharts@v11.4.0-XcqxbqWpy3Q67GvtWLRq/mode=imports/unoptimized/es-modules/masters/highcharts.src.js" # source: https://cdn.skypack.dev/highcharts/es-modules/masters/highcharts.src.js
-pin "highcharts-annotations", to: "https://cdn.skypack.dev/pin/highcharts@v11.4.0-XcqxbqWpy3Q67GvtWLRq/mode=imports/unoptimized/es-modules/masters/modules/annotations.src.js" # source: https://cdn.skypack.dev/highcharts/es-modules/masters/modules/annotations.src.js
-pin "highlight.js", to: "https://cdn.skypack.dev/pin/highlight.js@v11.9.0-bSUFWqJEEn57ytKpUEAb/mode=imports/optimized/highlightjs.js" # source: https://cdn.skypack.dev/highlight.js
+# use bin/importmap update to update these to the latest version
+pin "stimulus-autocomplete" # @3.1.0
+pin "el-transition" # @0.0.7
+
+# use rails importmap:check to check for major version changes
+pin "highcharts", to: "https://cdn.jsdelivr.net/npm/highcharts@11/es-modules/masters/highcharts.src.js"
+pin "highcharts-annotations", to: "https://cdn.jsdelivr.net/npm/highcharts@11/es-modules/masters/modules/annotations.src.js"
+pin "highlight.js", to: "https://cdn.jsdelivr.net/npm/highlight.js@11/+esm"
 
 pin_all_from "app/javascript/channels", under: "channels"
 pin_all_from "app/javascript/controllers", under: "controllers"
---
+++ lib/tasks/check_importmap.rake
@@ -0,0 +1,50 @@
+require "concurrent"
+
+class ImportMapChecker
+  attr_reader :file, :content, :mutex
+
+  def initialize
+    @file = Rails.root.join("config/importmap.rb")
+    @content = File.read(file)
+    @mutex = Mutex.new
+  end
+
+  def check
+    futures.each(&:value)
+  end
+
+  private
+
+  def futures
+    @futures ||= content.split("\n").filter_map { |line| create_future(line) }
+  end
+
+  def create_future(line)
+    return unless line.include?("jsdelivr")
+
+    Concurrent::Future.execute do
+      current_url = line[/to: "(.*)"/, 1]
+      non_versioned_url = current_url.sub(/@[\d]+\//, "/")
+      current_version = URI(current_url)
+      latest_version = URI(non_versioned_url)
+
+      current = Net::HTTP.get_response(URI(current_version))
+      if current.is_a?(Net::HTTPSuccess)
+        latest = Net::HTTP.get_response(URI(latest_version))
+        if latest.is_a?(Net::HTTPSuccess)
+          puts "There is a new major version of #{current_url} available" if current.body != latest.body
+        else
+          puts "Could not fetch #{latest_version}"
+        end
+      else
+        puts "Could not fetch #{current_version}"
+      end
+    end
+  end
+end
+
+namespace :importmap do
+  task check: :environment do
+    ImportMapChecker.new.check
+  end
+end
