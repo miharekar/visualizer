3d2124f6f5684851bb4a5d5f443aa013a743ff8e
Miha Rekar <info@mr.si>
2024-04-05 15:53:12 +0200

    Create enable_coffee_management_job.rb

---
+++ app/jobs/enable_coffee_management_job.rb
@@ -0,0 +1,40 @@
+class EnableCoffeeManagementJob < ApplicationJob
+  prepend MemoWise
+
+  queue_as :low
+
+  attr_reader :user
+
+  def perform(user)
+    @user = user
+    upsert_attributes = user.shots.where(coffee_bag_id: nil).map do |shot|
+      coffee_bag_id = coffee_bags["#{shot.bean_brand}_#{shot.bean_type}_#{shot.roast_date}"]
+      next unless coffee_bag_id
+
+      {id: shot.id, coffee_bag_id:}
+    end
+
+    Shot.upsert_all(upsert_attributes.compact, returning: false)
+  end
+
+  private
+
+  memo_wise def roasters
+    user.shots.distinct.pluck(:bean_brand)
+      .reject(&:blank?)
+      .to_h { |name| [name, Roaster.find_or_create_by(user:, name:)] }
+  end
+
+  memo_wise def coffee_bags
+    user.shots.distinct
+      .pluck(:bean_brand, :bean_type, :roast_date, :roast_level)
+      .reject { |bean_brand, bean_type, _, _| bean_brand.blank? || bean_type.blank? }
+      .to_h do |bean_brand, bean_type, string_roast_date, roast_level|
+        roast_date = Date.parse(string_roast_date) rescue nil
+        coffee_bag = roasters[bean_brand].coffee_bags.find_or_create_by(name: bean_type, roast_date:) do |bag|
+          bag.roast_level = roast_level
+        end
+        ["#{bean_brand}_#{bean_type}_#{string_roast_date}", coffee_bag.id]
+      end
+  end
+end
