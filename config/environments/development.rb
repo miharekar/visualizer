9f3243bd77b59a7c501ffb919bb42dbf4fe4ace9
Miha Rekar <info@mr.si>
2024-05-03 10:09:18 +0200

    Add tailscale

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
 
   config.hosts << /.+\.ngrok-free\.app/
   config.hosts << /.+\.test/
+  config.hosts << /.+\.platypus-koi.ts.net/
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # config.active_storage.service = :local
