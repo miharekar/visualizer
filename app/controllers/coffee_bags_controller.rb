ab2741196968c0a770a71738722e41042ac86f8b
Miha Rekar <info@mr.si>
2024-05-24 11:53:00 +0200

    Add search to coffee bags

--- app/controllers/coffee_bags_controller.rb
+++ app/controllers/coffee_bags_controller.rb
@@ -1,9 +1,13 @@
 class CoffeeBagsController < ApplicationController
   before_action :set_roaster
   before_action :set_coffee_bag, only: %i[edit update destroy remove_image]
+  before_action :load_coffee_bags, only: %i[index search]
 
   def index
-    @coffee_bags = @roaster.coffee_bags.by_roast_date
+  end
+
+  def search
+    render :index
   end
 
   def new
@@ -48,6 +52,11 @@ class CoffeeBagsController < ApplicationController
 
   def set_coffee_bag
     @coffee_bag = @roaster.coffee_bags.find(params[:id])
+  end
+
+  def load_coffee_bags
+    @coffee_bags = @roaster.coffee_bags.by_roast_date
+    @coffee_bags = @coffee_bags.where("coffee_bags.name ILIKE ?", "%#{ActiveRecord::Base.sanitize_sql_like(params[:coffee])}%") if params[:coffee].present?
   end
 
   def coffee_bag_params
