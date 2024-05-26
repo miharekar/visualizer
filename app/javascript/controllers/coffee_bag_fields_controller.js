4e3c85513cd7c24d0eb7e0c232e9b366ef187c04
Miha Rekar <info@mr.si>
2024-05-26 07:32:19 +0200

    Allow creation of new coffee bag from shot edit

--- app/javascript/controllers/coffee_bag_fields_controller.js
+++ app/javascript/controllers/coffee_bag_fields_controller.js
@@ -2,16 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 import { Turbo } from "@hotwired/turbo-rails"
 
 export default class extends Controller {
-  static targets = ["roasters"]
+  static targets = ["roasters", "coffeeBags"]
 
-  fetch() {
-    const roaster = this.roastersTarget.value
-    if (roaster === "new") {
+  roaster_changed() {
+
+    if (this.roastersTarget.value === "new") {
       Turbo.visit(this.element.dataset.newRoasterPath)
     } else {
       const frame = document.getElementById("coffee_bag_fields")
-      frame.src = this.roastersTarget.dataset.url + `&roaster=${roaster}`
+      let url = this.roastersTarget.dataset.url
+      const separator = url.includes('?') ? '&' : '?'
+      url += `${separator}roaster=${this.roastersTarget.value}`
+      frame.src = url
       frame.reload()
+    }
+  }
+
+
+  coffee_bag_changed() {
+    if (this.coffeeBagsTarget.value === "new") {
+      Turbo.visit(this.element.dataset.newCoffeeBagPath)
     }
   }
 }
