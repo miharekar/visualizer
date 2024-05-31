7fcdfec1a9acfc1451dec07ddd6a9cd837cc5f3b
Miha Rekar <info@mr.si>
2024-05-31 20:19:21 +0200

    Filter combo box and fix bugs

--- app/javascript/controllers/combobox_controller.js
+++ app/javascript/controllers/combobox_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
 
   connect() {
     this.shown = false
+    this.selected = this.listTarget.querySelector(".is-selected") || this.listTarget.querySelector("li")
   }
 
   show() {
@@ -12,8 +13,8 @@ export default class extends Controller {
     this.inputTarget.focus()
     this.listTarget.classList.remove("hidden")
     this.markAllAsInactive()
-    this.selected = this.listTarget.querySelector(".is-selected") || this.listTarget.querySelector("li")
     this.active = this.selected
+    this.filter()
   }
 
   hide(event) {
@@ -24,6 +25,12 @@ export default class extends Controller {
     this.listTarget.classList.add("hidden")
   }
 
+  windowClick(event) {
+    if (this.shown && !this.element.contains(event.target)) {
+      this.hide(event)
+    }
+  }
+
   blur(event) {
     if (event.relatedTarget && !this.element.contains(event.relatedTarget)) {
       this.hide(event)
@@ -31,37 +38,70 @@ export default class extends Controller {
   }
 
   toggle(event) {
-    this.shown ? this.hide(event) : this.show()
+    if (this.shown) {
+      this.hide(event)
+      this.inputTarget.value = this.selected.dataset.roasterName
+    } else {
+      this.shown = true
+      this.listTarget.classList.remove("hidden")
+      this.listTarget.querySelectorAll("li").forEach((el) => { el.classList.remove("hidden") })
+    }
   }
 
   highlight(event) {
     this.markAsActive(event.currentTarget)
   }
 
-  unhighlight(event) {
-    this.markAllAsInactive(event.currentTarget)
+  filter() {
+    const query = this.inputTarget.value.toLowerCase()
+    this.listTarget.querySelectorAll("li").forEach((el) => {
+      const name = el.dataset.roasterName.toLowerCase()
+      if (name.includes(query)) {
+        el.classList.remove("hidden")
+      } else {
+        el.classList.add("hidden")
+        if (this.active === el) {
+          this.active = null
+        }
+      }
+    })
+  }
+
+  findPreviousVisibleElement(element) {
+    while (element) {
+      element = element.previousElementSibling
+      if (element && !element.classList.contains('hidden')) {
+        console.log("here")
+        return element
+      }
+    }
+  }
+
+  findNextVisibleElement(element) {
+    while (element) {
+      element = element.nextElementSibling
+      if (element && !element.classList.contains('hidden')) {
+        return element
+      }
+    }
   }
 
   highlightPrevious(event) {
     if (!this.shown) this.show()
 
     event.stopPropagation()
-    const previous = this.active.previousElementSibling
-    if (previous) {
-      this.markAllAsInactive()
-      this.markAsActive(previous)
-    }
+    const previous = this.findPreviousVisibleElement(this.getActive()) || this.getActive()
+    this.markAllAsInactive()
+    this.markAsActive(previous)
   }
 
   highlightNext(event) {
     if (!this.shown) this.show()
 
     event.stopPropagation()
-    const next = this.active.nextElementSibling
-    if (next) {
-      this.markAllAsInactive()
-      this.markAsActive(next)
-    }
+    const next = this.findNextVisibleElement(this.getActive()) || this.getActive()
+    this.markAllAsInactive()
+    this.markAsActive(next)
   }
 
   markAsActive(element) {
@@ -80,19 +120,31 @@ export default class extends Controller {
 
   select(event) {
     if (!this.shown) return
-
     event.preventDefault()
+
+    let active = this.getActive()
+    if (!active) return
+    this.inputTarget.value = this.active.dataset.roasterName
+
+    if (active === this.selected) {
+      this.hide(event)
+      return
+    }
+
     this.listTarget.querySelectorAll("li").forEach((el) => { el.classList.remove("is-selected") })
 
     if (this.listTarget.contains(event.currentTarget)) {
       this.active = event.currentTarget
     }
 
-    this.selected = this.active
+    this.selected = active
     this.selected.classList.add("is-selected")
-    this.inputTarget.value = this.selected.dataset.roasterName
     this.hiddenInputTarget.value = this.selected.dataset.roasterId
     this.hiddenInputTarget.dispatchEvent(new Event("change"))
     this.hide(event)
+  }
+
+  getActive() {
+    return this.active || this.listTarget.querySelector("li:not(.hidden)")
   }
 }
