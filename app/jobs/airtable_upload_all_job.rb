f290b3c506cb5fa9bac62093eac94d6ad6f14e5b
Miha Rekar <info@mr.si>
2024-08-02 15:49:19 +0200

    Do a token check inside AirtableUploadAllJob

--- app/jobs/airtable_upload_all_job.rb
+++ app/jobs/airtable_upload_all_job.rb
@@ -1,13 +1,18 @@
 class AirtableUploadAllJob < AirtableJob
   def perform(user, shot_ids = nil)
-    Airtable::Base.prepare_for(user)
+    identity = user.identities.find_by(provider: "airtable")
+    if identity.valid_token?
+      Airtable::Base.prepare_for(user)
 
-    if user.coffee_management_enabled?
-      Airtable::Roasters.new(user).upload_multiple(user.roasters.where(airtable_id: nil))
-      Airtable::CoffeeBags.new(user).upload_multiple(CoffeeBag.where(roaster: user.roasters, airtable_id: nil).includes(:roaster))
+      if user.coffee_management_enabled?
+        Airtable::Roasters.new(user).upload_multiple(user.roasters.where(airtable_id: nil))
+        Airtable::CoffeeBags.new(user).upload_multiple(CoffeeBag.where(roaster: user.roasters, airtable_id: nil).includes(:roaster))
+      end
+
+      shots = shot_ids ? user.shots.where(id: shot_ids) : user.shots.where(airtable_id: nil)
+      Airtable::Shots.new(user).upload_multiple(shots.includes(:coffee_bag))
+    else
+      identity.refresh_token!
     end
-
-    shots = shot_ids ? user.shots.where(id: shot_ids) : user.shots.where(airtable_id: nil)
-    Airtable::Shots.new(user).upload_multiple(shots.includes(:coffee_bag))
   end
 end
