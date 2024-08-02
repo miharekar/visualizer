94bc46d30f1fa33c7416d9efabdd918c2b29d8ad
Miha Rekar <info@mr.si>
2024-08-02 13:19:20 +0200

    User ID is not on coffee bags

--- app/jobs/airtable_upload_record_job.rb
+++ app/jobs/airtable_upload_record_job.rb
@@ -3,7 +3,7 @@ class AirtableUploadRecordJob < AirtableJob
     record.class.airtable_class.new(record.user).upload(record)
   rescue Airtable::DataError => e
     Appsignal.set_error(e) do |transaction|
-      transaction.set_tags(record_class: record.class.name, record_id: record.id, user_id: record.user_id)
+      transaction.set_tags(record_class: record.class.name, record_id: record.id, user_id: record.try(:user_id))
     end
   end
 end
