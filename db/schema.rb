5f2f68d2fdbc1eaaf9f2555427875226b18fdb7d
Miha Rekar <info@mr.si>
2024-08-24 11:26:09 +0200

    Solid queue step 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_24_092339) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_24_092548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -286,8 +286,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_24_092339) do
     t.string "hostname"
     t.text "metadata"
     t.datetime "created_at", null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["last_heartbeat_at"], name: "index_solid_queue_processes_on_last_heartbeat_at"
+    t.index ["name", "supervisor_id"], name: "index_solid_queue_processes_on_name_and_supervisor_id", unique: true
     t.index ["supervisor_id"], name: "index_solid_queue_processes_on_supervisor_id"
   end
 
@@ -318,7 +319,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_24_092339) do
     t.text "arguments"
     t.string "queue_name"
     t.integer "priority", default: 0
-    t.boolean "static", default: true
+    t.boolean "static", default: true, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
