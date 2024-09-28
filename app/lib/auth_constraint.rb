a8f53c0b88d20b3c8681c6edcc18714c96c154d7
Miha Rekar <info@mr.si>
2024-09-28 09:03:49 +0200

    Update AuthConstraint to work with sessions

--- app/lib/auth_constraint.rb
+++ app/lib/auth_constraint.rb
@@ -1,6 +1,6 @@
 class AuthConstraint
   def self.admin?(request)
-    # TODO
-    false
+    cookies = ActionDispatch::Cookies::CookieJar.build(request, request.cookies)
+    User.joins(:sessions).where(sessions: {id: cookies.signed[:session_id]}, admin: true).exists?
   end
 end
