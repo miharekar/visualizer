065b009a21fdaa83c3b89ba90977d4f97533f70e
Miha Rekar <info@mr.si>
2024-11-27 18:41:06 +0100

    Fix SolidQueue logs not coming to AppSignal
    https://github.com/reidmorrison/rails_semantic_logger/issues/237

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,4 +2,10 @@ class ApplicationJob < ActiveJob::Base
   retry_on ActiveRecord::Deadlocked
 
   discard_on ActiveJob::DeserializationError, ActiveStorage::IntegrityError
+
+  before_perform :semantic_logger_sync
+
+  def semantic_logger_sync
+    SemanticLogger.sync!
+  end
 end
