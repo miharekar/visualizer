5264ea5c0e3b4ef1411d93c8a49d7a62565cc822
Miha Rekar <info@mr.si>
2024-12-31 14:00:10 +0100

    Try to improve service worker opening push notifications

--- app/views/pwa/service-worker.js
+++ app/views/pwa/service-worker.js
@@ -8,23 +8,5 @@ self.addEventListener("push", async (event) => {
 self.addEventListener("notificationclick", (event) => {
   event.notification.close()
   const url = new URL(event.notification.data?.path || "/", self.location.origin).href
-  event.waitUntil(openURL(url))
+  event.waitUntil(self.clients.openWindow(url).then(windowClient => windowClient?.focus()))
 })
-
-async function openURL(url) {
-  const clients = await self.clients.matchAll({ type: "window" })
-  const focused = clients.find((client) => client.focused)
-
-  if (focused) {
-    try {
-      const navigated = await focused.navigate(url)
-      if (!navigated) {
-        await self.clients.openWindow(url)
-      }
-    } catch (e) {
-      await self.clients.openWindow(url)
-    }
-  } else {
-    await self.clients.openWindow(url)
-  }
-}
