5ea0bb71dced2edb9361eeb61b8ba063ef8ec82b
Miha Rekar <info@mr.si>
2025-01-24 09:32:39 +0100

    Update push_subscription.rb

--- app/models/push_subscription.rb
+++ app/models/push_subscription.rb
@@ -1,3 +1,25 @@
 class PushSubscription < ApplicationRecord
   belongs_to :user
 end
+
+# == Schema Information
+#
+# Table name: push_subscriptions
+#
+#  id         :uuid             not null, primary key
+#  auth_key   :string
+#  endpoint   :string
+#  p256dh_key :string
+#  user_agent :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :uuid             not null
+#
+# Indexes
+#
+#  index_push_subscriptions_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
