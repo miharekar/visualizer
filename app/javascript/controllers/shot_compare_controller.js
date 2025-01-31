dafcd10b85706f6c9f6f15f60e21d34164bbdaed
Miha Rekar <info@mr.si>
2025-01-31 16:16:40 +0100

    Have a banner explaining what's happening and handle some edge cases

--- app/javascript/controllers/shot_compare_controller.js
+++ app/javascript/controllers/shot_compare_controller.js
@@ -1,17 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
+import { debounce } from "helpers/timing_helpers"
 
 export default class extends Controller {
-  static targets = ["checkbox"]
+  static targets = ["checkbox", "banner"]
 
-  connect() {
-    const checkbox = this.checkboxTargets.find(checkbox => checkbox.checked)
-    if (checkbox) {
-      this.select({ target: checkbox })
-    }
+  initialize() {
+    this.debouncedCheckState = debounce(() => this.checkState(), 10)
   }
 
-  checkboxTargetConnected(checkbox) {
-    this.toggleComparing(checkbox.closest(".group"))
+  connect() {
+    this.checkState()
+  }
+
+  checkboxTargetConnected() {
+    this.debouncedCheckState()
   }
 
   select(event) {
@@ -20,6 +22,7 @@ export default class extends Controller {
     this.checkboxTargets.forEach(checkbox => {
       this.toggleComparing(checkbox.closest(".group"))
     })
+    this.toggleBanner()
   }
 
   view(event) {
@@ -35,11 +38,29 @@ export default class extends Controller {
     }
   }
 
+  checkState() {
+    const checkbox = this.checkboxTargets.find(checkbox => checkbox.checked)
+    if (checkbox) {
+      this.select({ target: checkbox })
+    } else {
+      this.isChecked = false
+      this.toggleBanner()
+    }
+  }
+
   toggleComparing(shotRow) {
     if (this.isChecked) {
       shotRow.setAttribute('data-comparing', true)
     } else {
       shotRow.removeAttribute('data-comparing')
+    }
+  }
+
+  toggleBanner() {
+    if (this.isChecked) {
+      this.bannerTarget.classList.remove('hidden')
+    } else {
+      this.bannerTarget.classList.add('hidden')
     }
   }
 }
