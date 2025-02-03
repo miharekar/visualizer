00c8e9b4cd7671893f36ad0d8a757f1260d4afdf
Miha Rekar <info@mr.si>
2025-02-03 22:00:17 +0100

    Disable stripe webhook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,7 +2,7 @@ class WebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def stripe
-    StripeWebhookHandler.new(request).handle
+    # StripeWebhookHandler.new(request).handle
     head :ok
   rescue JSON::ParserError, Stripe::SignatureVerificationError
     head :bad_request
