1c2941f403c20bb2a0827fd58682e8ede4d656f7
Miha Rekar <info@mr.si>
2025-02-04 17:57:13 +0100

    Extract LemonSqueezy::Client

--- app/lib/lemon_squeezy.rb
+++ app/lib/lemon_squeezy.rb
@@ -3,97 +3,42 @@ require "json"
 require "uri"
 
 class LemonSqueezy
-  class APIError < StandardError; end
   class SignatureVerificationError < StandardError; end
-  BASE_URL = "https://api.lemonsqueezy.com/v1".freeze
 
-  attr_reader :api_key
+  class APIError < StandardError
+    attr_reader :code
 
-  def initialize
-    @api_key = Rails.application.credentials.lemon_squeezy.api_key
-  end
-
-  def get_customer(id)
-    make_request(:get, "/customers/#{id}")
-  end
-
-  def get_customers(params: {})
-    make_request(:get, "/customers", params:)
-  end
-
-  def get_subscription(id)
-    make_request(:get, "/subscriptions/#{id}")
-  end
-
-  def get_subscriptions(params: {})
-    make_request(:get, "/subscriptions", params:)
-  end
-
-  def all_subscriptions
-    paginate("/subscriptions")
-  end
-
-  def all_customers
-    paginate("/customers")
+    def initialize(message, code)
+      @code = code
+      super(message)
+    end
   end
 
   def create_checkout(data)
-    make_request(:post, "/checkouts", data:)
+    Client.new("/checkouts", method: :post, data:).make_request(:post, "/checkouts", data:)
   end
 
-  private
-
-  def paginate(path, results = [], current_page = 1, page_size = 100)
-    response = make_request(:get, path, params: {
-      page: {number: current_page, size: page_size}
-    })
-
-    new_results = results + response["data"]
-
-    if current_page >= response.dig("meta", "page", "lastPage")
-      new_results
-    else
-      paginate(path, new_results, current_page + 1, page_size)
-    end
+  def get_customer(id)
+    Client.new("/customers/#{id}").make_request
   end
 
-  def make_request(method, path, data: nil, params: {})
-    uri = URI.parse("#{BASE_URL}#{path}")
-
-    encoded_params = params.flat_map do |key, value|
-      if value.is_a?(Hash)
-        value.map { |k, v| ["#{key}[#{k}]", v] }
-      else
-        [[key, value]]
-      end
-    end
-
-    uri.query = URI.encode_www_form(encoded_params) unless encoded_params.empty?
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    response = http.request(build_request(method, uri, data))
-
-    if response.is_a?(Net::HTTPSuccess)
-      JSON.parse(response.body)
-    else
-      error = begin
-        JSON.parse(response.body)
-      rescue JSON::ParserError
-        nil
-      end
-      message = error&.dig("errors", 0, "detail") || response.message
-      raise APIError, "Lemon Squeezy API Error (#{response.code}): #{message}"
-    end
+  def get_customers(params: {})
+    Client.new("/customers", params:).make_request
   end
 
-  def build_request(method, uri, data)
-    request_class = Net::HTTP.const_get(method.to_s.capitalize)
-    request_class.new(uri).tap do |request|
-      request["Accept"] = "application/vnd.api+json"
-      request["Content-Type"] = "application/vnd.api+json"
-      request["Authorization"] = "Bearer #{api_key}"
-      request.body = data.to_json if data.present?
-    end
+  def get_subscription(id)
+    Client.new("/subscriptions/#{id}").make_request
+  end
+
+  def get_subscriptions(params: {})
+    Client.new("/subscriptions", params:).make_request
+  end
+
+  def all_subscriptions
+    Client.new("/subscriptions").paginate
+  end
+
+  def all_customers
+    Client.new("/customers").paginate
   end
 end
---
+++ app/lib/lemon_squeezy/client.rb
@@ -0,0 +1,88 @@
+class LemonSqueezy
+  class Client
+    prepend MemoWise
+
+    BASE_URL = "https://api.lemonsqueezy.com/v1".freeze
+
+    attr_reader :api_key, :uri, :request_class, :data, :params
+
+    def initialize(path, method: :get, data: nil, params: {})
+      @api_key = Rails.application.credentials.lemon_squeezy.api_key
+      @uri = URI.parse(BASE_URL + path)
+      @request_class = Net::HTTP.const_get(method.to_s.capitalize)
+      @data = data
+      @params = params
+      @results = []
+    end
+
+    def make_request(retrying: false)
+      handle_response(get_response)
+    rescue APIError => e
+      raise if retrying || e.code != 500
+
+      retrying = true
+      sleep(1)
+      retry
+    end
+
+    def paginate(current_page: 1, page_size: 100)
+      @params[:page] ||= {}
+      @params[:page] = @params[:page].merge(number: current_page, size: page_size)
+
+      response = make_request
+      @results += response["data"]
+
+      if current_page >= response.dig("meta", "page", "lastPage")
+        @results
+      else
+        paginate(current_page: current_page + 1, page_size:)
+      end
+    end
+
+    private
+
+    def encode_params(params)
+      params.flat_map do |key, value|
+        if value.is_a?(Hash)
+          value.map { |k, v| ["#{key}[#{k}]", v] }
+        else
+          [[key, value]]
+        end
+      end
+    end
+
+    def get_response
+      if params.present?
+        encoded_params = encode_params(params)
+        uri.query = URI.encode_www_form(encoded_params)
+      end
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.request(build_request)
+    end
+
+    def build_request
+      request_class.new(uri).tap do |req|
+        req["Accept"] = "application/vnd.api+json"
+        req["Content-Type"] = "application/vnd.api+json"
+        req["Authorization"] = "Bearer #{api_key}"
+        req.body = data.to_json if data.present?
+      end
+    end
+
+    def handle_response(response)
+      if response.is_a?(Net::HTTPSuccess)
+        JSON.parse(response.body)
+      else
+        error = begin
+          JSON.parse(response.body)
+        rescue JSON::ParserError
+          nil
+        end
+        message = error&.dig("errors", 0, "detail") || response.message
+        raise APIError.new(message, response.code.to_i), "Lemon Squeezy API Error (#{response.code}): #{message}"
+      end
+    end
+  end
+end
