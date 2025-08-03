84754457763dffcb1b6d9c7167c3d2af4f2e078a
Miha Rekar <info@mr.si>
2025-08-03 10:31:51 +0200

    Only have appsignal logger in production

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,3 +1,5 @@
-appsignal_logger = Appsignal::Logger.new("rails")
-Rails.logger.broadcast_to(appsignal_logger)
-Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
+if Rails.env.production?
+  appsignal_logger = Appsignal::Logger.new("rails")
+  Rails.logger.broadcast_to(appsignal_logger)
+  Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
+end
