3255e313e512bdfd5ea1de0a920f95b68cdbfe45
Miha Rekar <info@mr.si>
2025-08-03 09:50:07 +0200

    Improve the JS controller

--- app/javascript/controllers/canonical_selector_controller.js
+++ app/javascript/controllers/canonical_selector_controller.js
@@ -4,36 +4,23 @@ export default class extends Controller {
   static targets = ["input", "autocomplete", "roaster", "coffeeBag", "id"]
 
   connect() {
-    this.autocompleteTarget.addEventListener("search", this.changed.bind(this))
-    this.autocompleteTarget.addEventListener("change", this.changed.bind(this))
+    this.autocompleteTarget.addEventListener("change", this.toggleInputs.bind(this))
     this.autocompleteTarget.addEventListener("autocomplete.change", this.autocompleted.bind(this))
 
-    if (this.idTarget.value != "") {
-      this.disableInputs()
-    }
+    this.toggleInputs()
   }
 
   autocompleted(event) {
     this.roasterTarget.value = event.detail.selected.dataset.roaster
     this.coffeeBagTarget.value = event.detail.selected.dataset.coffeeBag
-    this.disableInputs()
+    this.toggleInputs()
   }
 
-  changed() {
-    this.enableInputs()
-  }
-
-  disableInputs() {
+  toggleInputs() {
+    const disabled = !!this.idTarget.value
     for (const el of [this.roasterTarget, this.coffeeBagTarget]) {
-      el.disabled = true
-      el.classList.add("opacity-50", "cursor-not-allowed")
-    }
-  }
-
-  enableInputs() {
-    for (const el of [this.roasterTarget, this.coffeeBagTarget]) {
-      el.disabled = false
-      el.classList.remove("opacity-50", "cursor-not-allowed")
+      el.disabled = disabled
+      el.classList.toggle("cursor-not-allowed", disabled)
     }
   }
 }
