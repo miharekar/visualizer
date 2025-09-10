14d9a77be9f15b9c3ae98124c5dfabec7fff4bca
Miha Rekar <info@mr.si>
2025-09-10 12:16:18 +0200

    Bump highcharts

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,8 +4,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
-pin "highcharts", to: "https://cdn.jsdelivr.net/npm/highcharts@12.3.0/esm/highcharts.js"
-pin "highcharts-annotations", to: "https://cdn.jsdelivr.net/npm/highcharts@12.3.0/esm/modules/annotations.js"
+pin "highcharts", to: "https://cdn.jsdelivr.net/npm/highcharts@12.4.0/esm/highcharts.js"
+pin "highcharts-annotations", to: "https://cdn.jsdelivr.net/npm/highcharts@12.4.0/esm/modules/annotations.js"
 
 pin "highlight.js" # downloaded from https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/es/highlight.min.js
 
@@ -15,7 +15,7 @@ pin "el-transition" # @0.0.7
 pin "match-sorter" # @8.1.0
 pin "remove-accents" # @0.5.0
 pin "stimulus-autocomplete" # @3.1.0
-pin "@yaireo/tagify", to: "@yaireo--tagify.js" # @4.35.3
+pin "@yaireo/tagify", to: "@yaireo--tagify.js" # @4.35.4
 
 pin_all_from "app/javascript/channels", under: "channels"
 pin_all_from "app/javascript/controllers", under: "controllers"
