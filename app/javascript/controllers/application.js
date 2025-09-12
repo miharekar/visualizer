0ee4adde2a74876d2bd454f770fb1ebf66e12d0d
Miha Rekar <info@mr.si>
2025-09-12 16:53:41 +0200

    Add appsignal monitoring to FE

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,12 @@
 import { Application } from "@hotwired/stimulus"
 import { Autocomplete } from "stimulus-autocomplete"
 
+import Appsignal from "@appsignal/javascript"
+const appsignal = new Appsignal({ key: "cc9d5ef7-ee02-49a4-8645-fde2bb614bc5" })
+import { installErrorHandler } from "@appsignal/stimulus"
+
 const application = Application.start()
+installErrorHandler(appsignal, application)
 application.register("autocomplete", Autocomplete)
 
 // Configure Stimulus development experience
