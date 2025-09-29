b2ab25873d6f8b7a10025cc1e496c341fffb9cb8
Miha Rekar <info@mr.si>
2025-09-29 11:52:04 +0200

    Bump deps

--- vendor/javascript/@appsignal--stimulus.js
+++ vendor/javascript/@appsignal--stimulus.js
@@ -1,4 +1,4 @@
-// @appsignal/stimulus@1.0.20 downloaded from https://ga.jspm.io/npm:@appsignal/stimulus@1.0.20/dist/esm/index.js
+// @appsignal/stimulus@1.0.21 downloaded from https://ga.jspm.io/npm:@appsignal/stimulus@1.0.21/dist/esm/index.js
 
 function n(n,r){var e=r.handleError;r.handleError=function(r,t,o){var i=n.createSpan((function(n){return n.setAction((o===null||o===void 0?void 0:o.identifier)||"[unknown Stimulus controller]").setTags({framework:"Stimulus",message:t}).setError(r)}));n.send(i);e&&typeof e==="function"&&e.apply(this,arguments)}}export{n as installErrorHandler};
 
