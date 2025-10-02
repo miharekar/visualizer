dabf0ab5f6892b15f902facc25bd7cf8a68090fb
Radan Skorić <radan.skoric@gmail.com>
2025-10-02 10:05:51 +0200

    Remove direction parameter
    If direction would be set to `:asc` then the filter would need
    to be flipped to `before..`, or rather, with better naming to
    `after..`. But direction is never changed anywhere so in the spirit
    of YAGNI it's better to simplify the code and just hardcode :desc.

--- app/controllers/concerns/cursor_paginatable.rb
+++ app/controllers/concerns/cursor_paginatable.rb
@@ -1,7 +1,7 @@
 module CursorPaginatable
-  def paginate_with_cursor(relation, items: 20, before: nil, by: :id, direction: :desc)
+  def paginate_with_cursor(relation, items: 20, before: nil, by: :id)
     relation = relation.where(by => ..before) if before.present?
-    relation = relation.order(by => direction).limit(items + 1).to_a
+    relation = relation.order(by => :desc).limit(items + 1).to_a
     cursor = relation.pop.public_send(by) if relation.size > items
 
     [relation, cursor]
