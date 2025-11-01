9e7af772a2fb8557c7a8f3764bfa27037081a4bc
Miha Rekar <info@mr.si>
2025-11-01 08:37:56 +0100

    Don't create new sessions if it's the same

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -36,7 +36,7 @@ module Authentication
   end
 
   def start_new_session_for(user)
-    user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
+    user.sessions.find_or_create_by!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
       Current.session = session
       cookies.signed.permanent[:session_id] = {value: session.id, httponly: true, same_site: :lax}
     end
