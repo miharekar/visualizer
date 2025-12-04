cbbbb98ca95e10b5458841517b965279540ec5a9
Miha Rekar <info@mr.si>
2025-12-04 11:14:36 +0100

    Do not trigger appsignal in dev

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,10 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 import { Autocomplete } from "stimulus-autocomplete"
-
 import Appsignal from "@appsignal/javascript"
-const appsignal = new Appsignal({ key: "cc9d5ef7-ee02-49a4-8645-fde2bb614bc5" }) // gitleaks:allow
 import { installErrorHandler } from "@appsignal/stimulus"
 
+const isProduction = document.documentElement.dataset.environment === "production"
+const appsignal = new Appsignal({ key: isProduction ? "cc9d5ef7-ee02-49a4-8645-fde2bb614bc5" : null }) // gitleaks:allow
 const application = Application.start()
 installErrorHandler(appsignal, application)
 application.register("autocomplete", Autocomplete)
