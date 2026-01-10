98362dadfa7b5b0921decf6e3a427bd01436968e
Miha Rekar <info@mr.si>
2026-01-10 08:53:46 +0100

    Handle invalid bytecode in timezone

--- app/models/current.rb
+++ app/models/current.rb
@@ -5,7 +5,7 @@ class Current < ActiveSupport::CurrentAttributes
   resets { Time.zone = nil }
 
   def set_timezone_from_cookie(cookie_zone)
-    zone = user&.timezone.presence || cookie_zone.presence
+    zone = user&.timezone.presence || cookie_zone.to_s.encode("UTF-8", invalid: :replace, undef: :replace, replace: "").strip.presence
     self.timezone = zone && ActiveSupport::TimeZone[zone] || ActiveSupport::TimeZone["UTC"]
     Time.zone = timezone
   end
