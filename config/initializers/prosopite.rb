2340edeba21728bf8df225395d1e043824cbbbb1
Miha Rekar <info@mr.si>
2026-01-22 08:32:06 +0100

    Ignore rate limits cache for n+1

--- config/initializers/prosopite.rb
+++ config/initializers/prosopite.rb
@@ -2,4 +2,5 @@ if Rails.env.local?
   require "prosopite/middleware/rack"
   Rails.configuration.middleware.use(Prosopite::Middleware::Rack)
   Prosopite.min_n_queries = 3
+  Prosopite.allow_stack_paths = [/action_controller\/metal\/rate_limiting\.rb/]
 end
