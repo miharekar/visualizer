fd114d28c2c5cbc2fa9b61127c959c9723d044be
Miha Rekar <info@mr.si>
2026-02-07 11:18:54 +0100

    Add CUD for CoffeeBag and Roaster

--- app/controllers/api/coffee_bags_controller.rb
+++ app/controllers/api/coffee_bags_controller.rb
@@ -1,23 +1,56 @@
 module Api
   class CoffeeBagsController < Api::BaseController
-    before_action :verify_read_access
-    before_action :find_roaster
+    before_action :check_premium!, only: %i[create update destroy]
+    before_action :verify_read_access, only: %i[index show]
+    before_action :verify_write_access, only: %i[create update destroy]
+    before_action :load_coffee_bag, only: %i[show update destroy]
 
     def index
-      coffee_bags = @roaster.coffee_bags.active_first.by_roast_date
+      coffee_bags = Current.user.coffee_bags.active_first.by_roast_date
+      coffee_bags = coffee_bags.where(roaster_id: params[:roaster_id]) if params[:roaster_id].present?
       coffee_bags, paging = paginate(coffee_bags)
       render json: {data: coffee_bags.map { {id: it.id, name: it.name} }, paging:}
     end
 
     def show
-      coffee_bag = @roaster.coffee_bags.find(params[:id])
-      render json: coffee_bag.to_api_json
+      render json: @coffee_bag.to_api_json
+    end
+
+    def create
+      coffee_bag = Current.user.coffee_bags.build(coffee_bag_params)
+      if coffee_bag.save
+        render json: coffee_bag.to_api_json, status: :created
+      else
+        render json: {error: coffee_bag.errors.full_messages.join(", ")}, status: :unprocessable_content
+      end
+    end
+
+    def update
+      if @coffee_bag.update(coffee_bag_params)
+        render json: @coffee_bag.to_api_json
+      else
+        render json: {error: @coffee_bag.errors.full_messages.join(", ")}, status: :unprocessable_content
+      end
+    end
+
+    def destroy
+      @coffee_bag.destroy!
+      render json: {success: true}
     end
 
     private
 
-    def find_roaster
-      @roaster = Current.user.roasters.find(params[:roaster_id])
+    def load_coffee_bag
+      @coffee_bag = Current.user.coffee_bags.find_by(id: params[:id])
+      render json: {error: "Coffee bag not found"}, status: :not_found unless @coffee_bag
+    end
+
+    def coffee_bag_params
+      cb_params = params.expect(coffee_bag: %i[name url canonical_coffee_bag_id roast_date frozen_date defrosted_date notes roaster_id] + CoffeeBag::DISPLAY_ATTRIBUTES)
+      return cb_params if params.dig(:coffee_bag, :roaster_id).blank?
+
+      cb_params[:roaster_id] = Current.user.roasters.find_by(id: params.dig(:coffee_bag, :roaster_id))&.id
+      cb_params
     end
   end
 end
--- app/controllers/api/roasters_controller.rb
+++ app/controllers/api/roasters_controller.rb
@@ -1,6 +1,9 @@
 module Api
   class RoastersController < Api::BaseController
-    before_action :verify_read_access
+    before_action :verify_read_access, only: %i[index show]
+    before_action :verify_write_access, only: %i[create update destroy]
+    before_action :check_premium!, only: %i[create update destroy]
+    before_action :load_roaster, only: %i[show update destroy]
 
     def index
       roasters = Current.user.roasters.order_by_name
@@ -9,8 +12,40 @@ module Api
     end
 
     def show
-      roaster = Current.user.roasters.find(params[:id])
-      render json: roaster.to_api_json
+      render json: @roaster.to_api_json
+    end
+
+    def create
+      roaster = Current.user.roasters.build(roaster_params)
+      if roaster.save
+        render json: roaster.to_api_json, status: :created
+      else
+        render json: {error: roaster.errors.full_messages.join(", ")}, status: :unprocessable_content
+      end
+    end
+
+    def update
+      if @roaster.update(roaster_params)
+        render json: @roaster.to_api_json
+      else
+        render json: {error: @roaster.errors.full_messages.join(", ")}, status: :unprocessable_content
+      end
+    end
+
+    def destroy
+      @roaster.destroy!
+      render json: {success: true}
+    end
+
+    private
+
+    def load_roaster
+      @roaster = Current.user.roasters.find_by(id: params[:id])
+      render json: {error: "Roaster not found"}, status: :not_found unless @roaster
+    end
+
+    def roaster_params
+      params.expect(roaster: %i[name website canonical_roaster_id])
     end
   end
 end
