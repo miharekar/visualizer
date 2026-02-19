e174e7400d10c3b0c4655b54745c0dd4ff92c40f
Miha Rekar <info@mr.si>
2026-02-19 17:08:05 +0100

    Yearly email

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -4,7 +4,7 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.with(user:).yearly_brew
   end
 
-  %i[black_friday cancelled_after_trial cancelled_premium upcoming_renewal].each do |email|
+  %i[black_friday cancelled_after_trial cancelled_premium upcoming_renewal yearly_ls_refund].each do |email|
     define_method(email) do
       user = User.order("RANDOM()").first
       UserMailer.with(user:).public_send(email)
