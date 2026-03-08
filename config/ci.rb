62e20a4313461451fb55cb696dac51412671aa29
Miha Rekar <info@mr.si>
2026-03-08 18:28:02 +0100

    Ignore highcharts for rustywind

--- config/ci.rb
+++ config/ci.rb
@@ -4,7 +4,7 @@ CI.run do
   step "Setup", "bin/setup --skip-server"
 
   step "Style: Ruby", "bin/rubocop"
-  step "Style: TailwindCSS", "rustywind --check-formatted ."
+  step "Style: TailwindCSS", "rustywind --ignored-files ./vendor/javascript/highcharts.js --check-formatted ."
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Importmap vulnerability audit", "bin/importmap audit"
