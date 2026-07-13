ffe899b1a8849b7604905f4c5db7066bf09f76ab
Miha Rekar <info@mr.si>
2026-07-13 10:52:19 +0200

    Retry transient web push failures

--- app/jobs/web_push_job.rb
+++ app/jobs/web_push_job.rb
@@ -2,6 +2,7 @@ class WebPushJob < ApplicationJob
   CONTACT_EMAIL = "mailto:info@visualizer.coffee".freeze
 
   queue_as :default
+  rescue_from(WebPush::TooManyRequests, WebPush::PushServiceError) { retry_transient_failure }
 
   def perform(push_subscription, title:, body:, path: "/")
     WebPush.payload_send(
@@ -15,7 +16,17 @@ class WebPushJob < ApplicationJob
         private_key: Rails.application.credentials.webpush.private_key
       }
     )
-  rescue WebPush::ExpiredSubscription, WebPush::PushServiceError, WebPush::InvalidSubscription, WebPush::Unauthorized
+  rescue WebPush::ExpiredSubscription, WebPush::InvalidSubscription, WebPush::Unauthorized
     push_subscription.destroy
+  end
+
+  private
+
+  def retry_transient_failure
+    return unless executions < 3
+
+    delay = executions**4
+    jitter = determine_jitter_for_delay(delay, self.class.retry_jitter)
+    retry_job(wait: delay + jitter + 2.seconds)
   end
 end
